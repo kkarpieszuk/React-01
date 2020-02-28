affbb066b53010196bfb0c590dce0f6f4bab9910
Konrad Karpieszuk <kkarpieszuk@gmail.com>
2020-02-28 14:22:24 +0100

    28 lut - delete products zrobione

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,5 +1,19 @@
 import React from 'react'
+import propTypes from 'prop-types'
 
-const Button = () => <button>test</button>
+
+const Button = ({size, children, onClick, id }) => {
+   const handleOnClick = () => { onClick(id) };
+
+   return (<button 
+            onClick={handleOnClick}>
+              {size}{children}
+           </button>)
+  }
+
+Button.propTypes = {
+  size: propTypes.string,
+  children: propTypes.string.isRequired,
+}
 
 export default Button;
